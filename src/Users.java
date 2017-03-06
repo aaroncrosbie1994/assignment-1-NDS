cfddd2d090002e6fc5e645aadcfd40ccf074af4b
aaron-crosbie <aaroncrosbie1994@gmail.com>
2017-03-06 13:34:23 +0000

    Added NullReturn exception

--- src/Users.java
+++ src/Users.java
@@ -7,4 +7,18 @@ public class Users{
     public void addUser(User user){
         users.add(user);
     }
+
+    public String getDestination(String username) throws NullReturn{
+        String ipAdd;
+        for(int i=0; i<users.size(); i++){
+            if(users.getUsername.equals(username)){
+                ipAdd = users.getIpAddress();
+            }
+        }
+        if(ipAdd == null){
+            throw new NullReturn("ERROR 404 no IP Address found!");
+            break;
+        }
+        return ipAdd;
+    }
 }
